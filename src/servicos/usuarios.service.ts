79473a8ad8058d1dd64c162ddecc0730f32c5bb4
Rodolfo Fagundes <rodolfo3841@gmail.com>
2019-05-27 23:46:41 -0300

    Post login

--- src/servicos/usuarios.service.ts
+++ src/servicos/usuarios.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from 'src/app/model/Usuario';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  url: string = 'http://localhost:3000'
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.http = http;
+    
+  }
+
+
+  login(user: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(`${this.url}/login`, user);
+  }
+
+
 }
